310d52fc6730f8468b9b387f5429deeb72ab4eae
thanhdang198 <dangtrongthanh1998@gmail.com>
2023-09-08 09:55:12 +0700

    remove firebase configure

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,7 @@
-import 'package:firebase_core/firebase_core.dart';
-import 'package:firebase_crashlytics/firebase_crashlytics.dart';
-import 'package:firebase_analytics/firebase_analytics.dart';
-import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_easyloading/flutter_easyloading.dart';
 import 'package:google_fonts/google_fonts.dart';
-import 'package:hive_flutter/hive_flutter.dart';
 import 'package:vietmap_map/constants/colors.dart';
 import 'package:vietmap_map/features/pick_address_screen/pick_address_screen.dart';
 import 'package:vietmap_map/features/routing_screen/bloc/routing_bloc.dart';
@@ -15,34 +10,11 @@ import 'package:vietmap_map/features/routing_screen/search_address.dart';
 import 'package:vietmap_map/features/search_screen/search_screen.dart';
 
 import 'constants/route.dart';
-import 'core/firebase_remote_config.dart';
 import 'features/map_screen/bloc/map_bloc.dart';
 import 'features/map_screen/maps_screen.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
-
-  await Firebase.initializeApp();
-  FirebaseAnalytics analytics = FirebaseAnalytics.instance;
-  await Hive.initFlutter();
-  FlutterError.onError = (errorDetails) {
-    FirebaseCrashlytics.instance.recordFlutterFatalError(errorDetails);
-  };
-  PlatformDispatcher.instance.onError = (error, stack) {
-    FirebaseCrashlytics.instance.recordError(error, stack, fatal: true);
-    return true;
-  };
-  try {
-    await FirebaseRemoteConfigure.initConfig();
-    await FirebaseRemoteConfigure.getAllRemoteConfigValues();
-  } catch (e) {
-    analytics.logEvent(name: 'error_init_firebase_remote_config');
-    try {
-      await FirebaseRemoteConfigure.getAllRemoteConfigValues();
-    } catch (e) {
-      // ignore: avoid_print
-    }
-  }
   runApp(MultiBlocProvider(
     providers: [
       BlocProvider(create: (context) => MapBloc()),
--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,18 +5,10 @@
 import FlutterMacOS
 import Foundation
 
-import firebase_analytics
-import firebase_core
-import firebase_crashlytics
-import firebase_remote_config
 import geolocator_apple
 import path_provider_foundation
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
-  FLTFirebaseAnalyticsPlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseAnalyticsPlugin"))
-  FLTFirebaseCorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseCorePlugin"))
-  FLTFirebaseCrashlyticsPlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseCrashlyticsPlugin"))
-  FLTFirebaseRemoteConfigPlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseRemoteConfigPlugin"))
   GeolocatorPlugin.register(with: registry.registrar(forPlugin: "GeolocatorPlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
 }
